706d43271c113caaa5bda89651a5817ee4bbd844
Rhian Davies <thetrianglegirl@gmail.com>
2025-04-16 16:18:03 +0100

    👀 Set up a PR preview workflow

---
+++ .github/workflows/install_system_deps.sh
@@ -0,0 +1,4 @@
+# System dependencies required for {sf}
+sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update
+sudo apt-get install libudunits2-dev libgdal-dev libgeos-dev libproj-dev libsqlite3-dev
